4d4e11f116015e5d3716417b0952cfda3a8047c1
Cintra Guilherme <cinguilherme@gmail.com>
2021-10-30 09:48:49 -0300

    fix update user imports

--- src/service/users/http-in/handlers/updateUsersHandler.ts
+++ src/service/users/http-in/handlers/updateUsersHandler.ts
@@ -1,11 +1,11 @@
 import {Request, Response} from 'express';
-import {createUsers} from "../../models/users";
+import {createUser} from "../../models/users";
 import {User} from "../../schemas/UserSchema";
 
 export const updateUsersHandler = async (req: Request, res: Response) => {
     const user: User = req.body.username;
 
-    const updated = await createUsers(user);
+    const updated = await createUser(user);
 
     res.status(200);
     res.send({
