73218fabd9c8d0002aae47d7943ea1bbff4799df
cinguilherme <cinguilherme@gmail.com>
2021-10-30 23:23:17 -0300

    wip api key impl dummy api key validation

--- src/service/common/http-in/apiKeyValidator.ts
+++ src/service/common/http-in/apiKeyValidator.ts
@@ -2,8 +2,21 @@ import { Request, Response } from "express";
 
 export const apiKeyValidator = (req: Request, res: Response, next: Function) => {
 
-    console.log(req.headers);
-    
+    const key = req.headers.api_key;
 
-    next()
+    if (validKey(key)) {
+        next()
+    } else {
+        res.status(401)
+        res.send({ message: "api key required for this api" })
+    }
+
+}
+
+const validKey = (key: string | string[] | undefined) => {
+    if (key === undefined || typeof (key) !== 'string') {
+        return false
+    }
+    if (key === '1234567') return true
+    return false
 }
